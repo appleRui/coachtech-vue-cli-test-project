32b1a14dfdfc600163d0a196bbc46b8c9baa3396
appleRui <apple31581@gmail.com>
2022-04-14 08:43:10 +0900

    Vue Router SPAの基本

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
+import ManufactureList from "../views/ManufactureList.vue";
+// import AboutView from "../views/AboutView.vue";
 
 Vue.use(VueRouter);
 
@@ -12,13 +13,18 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: AboutView,
+    path: "/manufacture-list",
+    name: "ManufactureList",
+    component: ManufactureList,
   },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: AboutView,
+  // },
 ];
 
 const router = new VueRouter({
