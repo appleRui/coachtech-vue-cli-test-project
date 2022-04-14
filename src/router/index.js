2a3518db5d46fe493da7e175e7b7176364651f10
appleRui <apple31581@gmail.com>
2022-04-14 09:54:28 +0900

    サンプルアプリケーション　コーディング

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,14 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/about/:city",
+    name: "About",
     component: AboutView,
+    props: true,
   },
 ];
 
